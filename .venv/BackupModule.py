84fde827ca0754b2ac8711d6a70340f8859754e1
kwjay <kwjay1002@gmail.com>
2024-04-08 19:04:58 +0200

    Copying to folder

--- .venv/BackupModule.py
+++ .venv/BackupModule.py
@@ -21,16 +21,21 @@ class BackupModule:
 
 
     def start_backup(self):
-        print(self.backup_dir)
-        if not os.path.exists(self.backup_dir):
+        if not os.path.isdir(self.backup_dir):
             os.mkdir(self.backup_dir)
         with os.scandir(self.device_dir) as directory:
             for file in directory:
                 file_stat = os.stat(file.path)
+                file_size = file_stat.st_size
                 file_date = dt.fromtimestamp(file_stat.st_mtime)
                 year_dir = self.backup_dir + '\\' + str(file_date.year)
-                if not os.path.exists(year_dir):
+                if not os.path.isdir(year_dir):
                     os.mkdir(year_dir)
                 month_dir = year_dir + '\\' + file_date.strftime("%b")
-                if not os.path.exists(month_dir):
+                if not os.path.isdir(month_dir):
                     os.mkdir(month_dir)
+                file_destination = month_dir + '\\' + file.name
+                if not os.path.isfile(file_destination):
+                    shutil.copy(file.path, file_destination)
+                    print(file_destination)
+
